34c8889c84c49e7d60ff4a0bfad81dcc301548dd
xkatjusha <katharina.maier.25@web.de>
2022-12-13 13:04:08 +0100

    up to date

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+  purge:[
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
-  purg:[],
   darkMode: false,
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
